d5db8bd49a808cba3bbc652c3d785dfa65e9dafc
ArmanAgwan <armanagwa@gmail.com>
2024-03-12 23:33:05 +0530

    Command Line

---
+++ 01-Command_Line.js
@@ -0,0 +1,120 @@
+
+            //------------Command Line------------//
+
+
+    //Diving in
+        // --> Terminal and Command line
+        // --> Vim
+        // --> Shells
+
+        
+      // --> Terminal and Command line
+
+    /*
+        * ls = list directory contents
+        * cd = change directory 
+        * mkdir = Make directory 
+        * rmdir = Remove directory 
+        
+         
+        * cat = Show file contents
+        * man = command manual
+        * less = Show file content by page
+        * touch = creates an Empty File
+        * rm = remove File
+        * echo = repeat input
+
+    */
+
+         //-----Exercise------//
+
+/*
+    1.Navigate to your home directory
+    2.Make a new Directory called "temp"
+    3.Move into temp
+    4.List the directory contents
+    5.Make a file called "hello"
+    6.List the directory contents
+    7.Move out of temp
+    8.Delete temp
+*/
+
+        //---- In case you get lost
+
+/*
+    1. cd.. = Move up one directory
+    2. pwd = print working directory
+    3. clear = clear terminal output
+    4. ctrl + c = Exit program
+    5. cd ~ = navigate to home directory
+*/
+
+
+
+            //------------------VIM--------------//
+
+/*
+    insert mode         -       Text Editing
+
+    normal mode         -       Primary Mode
+
+    command mode        -       Searching , Saving , Exiting
+
+*/            
+
+/*                                  
+                            [Save and Exit :wq]
+
+    insert mode         -          i
+
+    normal mode         -          ESC
+
+    command mode        -           :
+
+*/            
+
+//--------Exercise--------// 
+
+/*
+    1.Navigate to your home directory
+    2.Make a directory called "temp"
+    3.Open a file named "test" with vi
+    4.Write a few lines
+    5.Save and Exit
+*/
+
+// ESC  :q!
+
+            //----------SHELL---------//
+
+
+//Show your current shell
+/*
+echo $0
+echo $SHELL
+*/
+
+
+/*
+    zsh - more extensible version of bash
+    ~/.zshrc
+*/
+
+/*
+    bash - Bourne again shell
+    ~/.bash_profile
+*/
+
+//Reload shell
+/*
+source <shell config location>
+*/
+
+
+//---------Shell Exercise----------// 
+
+/*
+    1.Open your shell config
+    2.Add a line to make your shell tell you 
+      good morning.
+*/
