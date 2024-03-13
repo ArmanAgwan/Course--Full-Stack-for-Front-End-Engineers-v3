4bc654401d1da9a2503f133c7f6120032267d094
ArmanAgwan <armanagwa@gmail.com>
2024-03-13 22:00:24 +0530

    Server

---
+++ 02-Server.js
@@ -0,0 +1,23 @@
+                     //----------Server----------//
+
+//    What is a server
+//    Cloud Computing
+//    VPS
+//    Operating System
+//    SSH
+
+
+                    //-------Basic Node Service--------//
+
+
+//   const http = require("http");
+//   const fs = require('fs');       ---> fs - file system
+//   const PORT = 3000;
+
+//   const server = http.createServer(function (req, res) {
+//       res.writeHead(200, {'content-type': 'text/html'});
+//       fs.createReadStream('index.html').pipe(res);
+//   });
+
+//   server.listen(PORT);
+//   console.log(`Server started on port ${PORT}`);
