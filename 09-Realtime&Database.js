b9b6196eff39cf508afee928370820cfb2a993b8
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-04-01 09:55:50 +0530

    Realtime

--- 09-Realtime&Database.js
+++ 09-Realtime&Database.js
@@ -1,25 +1,25 @@
-// //----------Realtime & Database----------//
+//----------Realtime & Database----------//
 
-// * Websockets
-// * Files
-// * Databasees
+* Websockets
+* Files
+* Databasees
 
-// //----------Websockets----------//
+//----------Websockets----------//
 
-// 1- Update nginx configuration
-// 2- Create a new nodejs server
-// 3- Enable websockets 
+1- Update nginx configuration
+2- Create a new nodejs server
+3- Enable websockets 
 
-// ---> Websockets: Persistent bidirection connection between client
-//                  and server. 
+---> Websockets: Persistent bidirection connection between client
+                 and server. 
 
 
-// location / {
-//    proxy_set_header Upgrade $http_upgrade:
-//    proxy_set_header Connection "upgrade";
+location / {
+   proxy_set_header Upgrade $http_upgrade:
+   proxy_set_header Connection "upgrade";
 
-//    proxy_pass http://127.0.0.1:3000;
-// }    
+   proxy_pass http://127.0.0.1:3000;
+}    
 
 
 const express = require('express');
@@ -32,3 +32,38 @@ app.get('/', function(req, res) {
 
 server.on('request', app);
 server.listen(3000, function () {console.log('Listening on 3000'); });
+
+
+//----------Database----------//
+
+       -----Relational-----
+
+* SQL
+* tables
+* Realated data
+* Strict structure
+
+      -----Non-relational-----
+
+* No SQL
+* Data agnostic
+* Loose structure
+
+
+        
+      USERS                                  PETS
+                                                
+     userID     --------------------->       petID
+      name   ----|                            userID
+     localtion   |                             name   
+      email      |                             type 
+                 |
+                 |
+                 |
+                 |------>       FOOD
+                                
+                               foodID
+                               userID
+                                name
+                                type
+                               spicy 
