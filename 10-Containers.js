55a0f84f083c8d7d1d559af80a0e89a1dab7193d
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-04-02 21:27:00 +0530

    Containers

--- 10-Containers.js
+++ 10-Containers.js
@@ -30,3 +30,26 @@
 // * Amazon EKS
 // * Apache Mesos
 // * AKS
+
+
+//----------Load Balancers----------//
+
+----------SCHEDULING ALGORITHMS----------
+
+1- Round Robin 
+2- IP Hashing 
+3- Random Choice 
+4- Least Connections 
+5- Least Load
+
+upstream backend {
+    server backend1.example.com;
+    server backend2.example.com;
+    server 192.0.0.1 backup;
+}
+
+server {
+    location / {
+        proxy_pass http://backend;
+    }
+}
